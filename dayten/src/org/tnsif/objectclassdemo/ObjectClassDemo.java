5ada3d9b743397ac42a29d54c16ee27b28f29b70
rawat <rawatabhinav012@gmail.com>
2023-05-19 16:12:10 +0530

    tenth commit1

---
+++ dayten/src/org/tnsif/objectclassdemo/ObjectClassDemo.java
@@ -0,0 +1,33 @@
+package org.tnsif.objectclassdemo;
+
+class Sample{
+	
+}
+
+class Demo{
+	
+	//constructor
+	Demo(){
+		//resource allocate
+		System.out.println("Inside constructor");
+	}
+	protected void finalize() {
+		System.out.println("In finalise method");
+	}
+}
+public class ObjectClassDemo {
+
+	public static void main(String[] args) {
+		Sample s= new Sample();
+		System.out.println(s.getClass());
+		System.out.println(s.hashCode());
+		Demo d = new Demo();
+		System.out.println(d.getClass());
+		System.out.println(d.hashCode());
+		
+		Object obj = new Object();
+		System.out.println(obj.getClass());
+
+	}
+
+}
